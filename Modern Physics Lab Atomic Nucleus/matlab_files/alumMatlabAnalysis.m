6bbc8e841decc05e0faea6d6a6ee574f6c739299
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-09 17:24:57 +0200

    plot of aluminum vs NIST finished
    Now need to make plot better and do the same for plastic values

---
+++ Modern Physics Lab Atomic Nucleus/matlab_files/alumMatlabAnalysis.m
@@ -0,0 +1,88 @@
+clear all
+clc
+close all
+myPath = ['/Users/kevin/SkyDrive/KTH Work/Period 3'...
+   '2014/SH2008/Atomic Nucleus Revised/'];
+addpath(genpath(myPath));
+%% Aluminum data
+densityOfAluminum = 2.70; % g/cm^3
+thickAlum = 0.000054; % thickness in meters
+expData=[... % # of sheets, energy in (keV)
+0,624.259; 
+1,601    ;
+2,577.5  ;
+3,550.5  ;
+4,505.5  ;
+5,470.5  ;
+6,450.5 ];
+y.one=expData(:,2)';
+x.one=expData(:,1)';
+sigma.one = [...
+4,
+4,
+1.5,
+7.5,
+1.5,
+8.5,
+10.5]';
+[a_fit.one, sig_a.one, yy.one, chisqr.one] = ...
+    linreg(x.one,y.one,sigma.one);
+M=2;
+N = length(x);
+%% * Print out the fit parameters, including their error bars.
+fprintf('Fit parameters:\n');
+for i=1:M
+  fprintf(' a(%g) = %g +/- %g \n',i,a_fit.one(i),sig_a.one(i));
+end
+
+%% * Graph the data, with error bars, and fitting function.
+figure(1); clf;           % Bring figure 1 window forward
+errorbar(x.one,y.one,sigma.one,'o'); % Graph data with error bars
+hold on;                  % Freeze the plot to add the fit
+plot(x.one,yy.one,'-');           % Plot the fit on same 
+% graph as data
+xlabel('x_i'); ylabel('y_i and Y(x)');
+title(['\chi^2 = ',num2str(chisqr.one),'  N-M = ',num2str(N-M)]);
+fprintf('  y(x) = %g + %g x \n',a_fit.one(1),a_fit.one(2));
+%% Find Zero of y(x)
+zero.x = abs(a_fit.one(1)/a_fit.one(2)); % energy is zero when
+% x is _. x is in units of # of sheets of aluminum multiply
+% (# of sheets)
+% by sheet thickness to get total sheet thickness when it should
+% stop the electrons
+zero.thickness = zero.x * thickAlum;
+%% Define R(E) = kE^B
+R.meters = zero.thickness - x.one * thickAlum; % in meters
+R.centimeters = R.meters * 100; % in cm
+R.gcm2 = R.centimeters * densityOfAluminum; % in g/cm^2
+E.keV = y.one; % in keV
+E.MeV = E.keV * 1e-3; % in MeV
+y.two = log10(R.gcm2);
+x.two = log10(E.MeV);
+sigma.two = (sigma.one*expData(:,1)./expData(:,2))';
+%%
+[a_fit.two, sig_a.two, yy.two, chisqr.two] = ...
+    linreg(x.two,y.two,sigma.two);
+fprintf('Fit parameters:\n');
+fprintf(' b = %g +/- %g \n',a_fit.two(1),sig_a.two(1));
+fprintf(' m = %g +/- %g \n',a_fit.two(2),sig_a.two(2));
+fprintf('ln(k)=b and B=m \n');
+fprintf(' R(E)=kE^B\n');
+fprintf(' R(E)=%g E^(%g)\n',10^(a_fit.two(1)),a_fit.two(2));
+%% * Graph the data, with error bars, and fitting function.
+figure(2); clf;           % Bring figure 1 window forward
+plotOf.OurErrorBars = errorbar(10.^x.two,...
+    10.^y.two,sigma.two,'o');  % Graph data with error bars
+hold on;                  % Freeze the plot to add the fit
+plotOf.OurFit = plot(10.^x.two,10.^yy.two,'-');
+% Plot the fit on same graph as data
+xlabel('x_i'); ylabel('y_i and Y(x)');
+title(['\chi^2 = ',num2str(chisqr.two),'  N-M = ',num2str(N-M)]);
+fprintf('  y(x) = %g + %g x \n',a_fit.two(1),a_fit.two(2));
+%% NIST
+[Kinetic,CSDA] = importNISTdataNow('edataCSDA_Range.txt',6, 86);
+startNist = 23;
+endNist = 30;
+plotOf.NIST=plot(Kinetic(startNist:endNist),...
+CSDA(startNist:endNist));
+hold off;
--- Modern Physics Lab Atomic Nucleus/matlab_files/chi_squared.m
+++ Modern Physics Lab Atomic Nucleus/matlab_files/chi_squared.m
@@ -3,28 +3,47 @@ clc
 myPath = ['/Users/kevin/SkyDrive/KTH Work/Period 3'...
     ' 2014/SH2008/Atomic Nucleus Revised/'];
 addpath(genpath(myPath));
+cd(myPath);
+%% Aluminum data
+thickAlum = 0.000054; % thickness in meters
+expData=[... % plate length in meters, energy in (keV)
+0,624.259; 
+1,601    ;
+2,577.5  ;
+3,550.5  ;
+4,505.5  ;
+5,470.5  ;
+6,450.5 ];
 %% Start
 densityOfAluminum = 1.13; % mg/cm^3
-y = log(1.13*1000*(0.204748-[0 0.0054 0.0108 0.0162 0.0216 0.027 0.0324]));
-x = log(1/1000*[624.259 601 577.5 550.5 505.5 470.5 450.5]);
-sigmaE = [3, 4, 1.5, 7.5, 1.5, 8.5, 10.5];
-sigmay = log([1.3764 2.9822 2.5234 2.9822 4.1292 2.0646 5.9644]);
+% ln(Range) = y
+plateLength = [0 0.000054 ... % in meters
+0.000108 0.000162 0.000216 0.00027 0.000324];
+y = log10(1.13*1000*(plateLength*100));
+% ln(E) = x
+x = log10(1/1000*[624.259 601 ...
+    577.5 550.5 505.5 470.5 450.5]);
+sigmaX = log10(1/1000*[3, 4, 1.5, 7.5, 1.5, 8.5, 10.5]);
+sigmaY = log10([1.3764 2.9822 ...
+    2.5234 2.9822 4.1292 2.0646 5.9644]);
 N = length(y);
 M = 2;
 %%
-[a_fit, sig_a, yy, chisqr] = linreg(x,y,sigmay);
+[a_fit, sig_a, yy, chisqr] = linreg(x,y,sigmaY);
 %%
 %% * Print out the fit parameters, including their error bars.
 fprintf('Fit parameters:\n');
-for i=1:M
-  fprintf(' a(%g) = %g +/- %g \n',i,a_fit(i),sig_a(i));
-end
+fprintf(' b = %g +/- %g \n',a_fit(1),sig_a(1));
+fprintf(' m = %g +/- %g \n',a_fit(2),sig_a(2));
+fprintf('ln(k)=b and B=m \n');
+fprintf(' R(E)=kE^B\n');
+fprintf(' R(E)=%g E^(%g)\n',10^(a_fit(1)),a_fit(2));
 %% * Graph the data, with error bars, and fitting function.
 figure(1); clf;           % Bring figure 1 window forward
-errorbar(x,exp(y),sigmay,'o');  % Graph data with error bars
+errorbar(x,10.^y',sigmaY,'o');  % Graph data with error bars
 hold on;                  % Freeze the plot to add the fit
 %%
-plot(x,exp(yy),'-');           % Plot the fit on same graph as data
+plot(x,10.^(yy),'-');      % Plot the fit on same graph as data
 xlabel('x_i'); ylabel('y_i and Y(x)');
 title(['\chi^2 = ',num2str(chisqr),'    N-M = ',num2str(N-2)]);
 %%
@@ -32,4 +51,12 @@ figure(2)
 clf;
 [Kinetic,CSDA] = importNISTdataNow('edataCSDA_Range.txt',6, 86);
 % CSDA = 1000*CSDA;
-plot(Kinetic,CSDA)
+plot(log10(Kinetic),log10(CSDA))
+%%
+figure(3)
+clf;
+plot(x,10.^(yy),'-'); 
+hold on;
+startN = 42;
+finishN = 55;
+plot(log(Kinetic(startN:finishN)),CSDA(startN:finishN));
