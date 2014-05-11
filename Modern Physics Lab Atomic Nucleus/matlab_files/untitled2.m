e7443876697e28180678a89e054ede89428e60e5
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-11 15:22:56 +0200

    save before toying with text mate

---
+++ Modern Physics Lab Atomic Nucleus/matlab_files/untitled2.m
@@ -0,0 +1,18 @@
+
+function output = untitled2(in)
+% UNTITLED2 Description
+%	OUTPUT = UNTITLED2(IN)
+%
+% Long description
+%%
+clear all;close all;clc
+a
+range = 1:10;
+c = -pi:0.1:pi;
+for i = range
+	x(i) = i;
+end
+disp(x);
+
+
+end % function
