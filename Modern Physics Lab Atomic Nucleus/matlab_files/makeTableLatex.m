5be951fa63369d79b83f6d0bbf499bd36cb6bb0d
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-13 01:28:25 +0200

    Final at 1:30 on monday...

--- Modern Physics Lab Atomic Nucleus/matlab_files/makeTableLatex.m
+++ Modern Physics Lab Atomic Nucleus/matlab_files/makeTableLatex.m
@@ -1,4 +1,4 @@
-function makeTableLatex( firstColumn,secondColumn,thirdColumn,fourthColumn,tableName )
+function makeTableLatex( firstColumn,secondColumn,thirdColumn,fourthColumn,fifthColumn,sixthColumn,tableName )
 %makeTableLatex( firstColumn,secondColumn,thirdColumn,fourthColumn,tableName )
 % Summary of this function goes here
 %   Detailed explanation goes here
@@ -6,21 +6,32 @@ FID = fopen(tableName, 'w');
 %%
 fprintf(FID, '\\begin{table}[h] \n');
 fprintf(FID, '   \\begin{center} \n');
-fprintf(FID, '      \\begin{tabular}{llll}\\toprule \n');
-fprintf(FID, '\\multicolumn{2}{c}{Aluminum} & \\multicolumn{2}{c}{Plastic}\\\\ \n');
-			fprintf(FID,'\\cmidrule(r){1-2} \n'); 
-			fprintf(FID,'\\cmidrule(r){3-4} \n');
-			fprintf(FID,'$\\delta R(E)$ [$\\frac{\\text{mg}}{\\text{cm}^2}$] & $\\delta E$ [keV] & $\\delta R(E)$ [$\\frac{\\text{mg}}{\\text{cm}^2}$] & $\\delta E$ [keV]\\\\ \\midrule \n');
+fprintf(FID, '      \\begin{tabular}{llllll}\\toprule \n');
+fprintf(FID, '\\multicolumn{3}{c}{Aluminum} & \\multicolumn{3}{c}{Plastic}\\\\ \n');
+fprintf(FID,'\\cmidrule(r){1-3} \n');
+fprintf(FID,'\\cmidrule(r){4-6} \n');
+fprintf(FID,[...
+    '$\\delta R(E)$ [$\\frac{\\text{mg}}{\\text{cm}^2}$]',...
+    '& $\\delta E$ [keV]',...
+    '& $\\delta R/R$ &',...
+    '$\\delta R(E)$ [$\\frac{\\text{mg}}{\\text{cm}^2}$]',...
+    '& $\\delta E$ [keV]',...
+    '& $\\delta R/R$',...
+    '\\\\ \\midrule \n']);
 for k=1:length(firstColumn)
-    fprintf(FID, '%8.2f & %8.2f & %8.2f & %8.2f \\\\ ', firstColumn(k), secondColumn(k), thirdColumn(k), fourthColumn(k));
+    fprintf(FID, '%8.2f & %8.2f & %8.2f & %8.2f & %8.2f & %8.2f \\\\ ', firstColumn(k), secondColumn(k), thirdColumn(k), fourthColumn(k), fifthColumn(k),sixthColumn(k));
     if k==length(firstColumn)
         fprintf(FID, '\\bottomrule ');
     end
     fprintf(FID, '\n');
 end
- fprintf(FID, '      \\end{tabular} \n');
+fprintf(FID, '      \\end{tabular} \n');
 fprintf(FID, '   \\end{center}\n');
-fprintf(FID,'\\caption{The uncertainties in the range is calculated from our uncertainties in energy peaks using equation,\\cite{093570275X} $\\delta R(E) = |k| \\delta E$. This equates to about 1 percent uncertainty.} \n');
+fprintf(FID,['\\caption{The uncertainties in the range is calculated from',...
+    'our extrapolation of the fit and',...
+    ' Equation~\\eqref{eq:transformationEquations}',...
+    ' $\\delta y = \\delta R/R$. The values obtained are',...
+    ' around $1\\perc~$to$~2\\perc$, agreeing with Equation~\\eqref{eq:bergerUncertainty}. } \n']);
 fprintf(FID,'\\label{tab:uncertaintyConversion} \n')
 fprintf(FID, '\\end{table} \n');
 fclose(FID);
